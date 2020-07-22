2d86705c69a9444edb72044689acd5641ba8138b
Ron Geva <coolron54@gmail.com>
2020-07-22 11:25:36 +0300

    Initial commit

---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MyComponenet extends React.Component{
+    render() {
+        return <h1>This is my component! :)</h1>;
+    }
+}
+
+export default MyComponenet;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MyComponenet from "./MyComponent";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <MyComponenet></MyComponenet>
   </React.StrictMode>,
   document.getElementById('root')
 );
