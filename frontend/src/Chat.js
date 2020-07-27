70ea42bd0b1c5fb359bd61175e07830cf6fdc19c
Ron Geva <coolron54@gmail.com>
2020-07-28 00:44:35 +0300

    Added basic implementation to chat component

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './Chat.css'
 
+
 class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chatInputValue: "",
+            chatBox: "This is the chatbox\n"
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput({ target }) {
+        this.setState({
+            chatBox: this.state.chatBox + this.state.chatInputValue  + "\n"
+        })
+    }
+
+    handleChange({ target }) {
+        this.setState({ chatInputValue: target.value });
+    }
+
     render() {
-        return <div className="chatDiv">
-            This is the chat
+        return <div className="chatDiv border">
+            Chat window
+            <br/>
+            <textarea disabled id="chatBox" value={ this.state.chatBox }>
+            </textarea>
+            <form>
+                <label>
+                    message:
+                    <input type="text" name="name" onChange={ this.handleChange }/>
+                </label>
+                <input type="button" value="send" onClick={ this.handleInput }/>
+            </form>
         </div>;
     }
 }
